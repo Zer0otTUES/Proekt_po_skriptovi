d310ead29d3e716f4c85e30f5117ddaaef97b8ac
Zer0otTUES <101138604+Zer0otTUES@users.noreply.github.com>
2023-06-13 21:03:13 +0300

    Add files via upload

--- website/app.py
+++ website/app.py
@@ -1,95 +1,136 @@
-from flask import Blueprint, render_template, request
-import time
-import io
+
 import base64
+import datetime as dt
+import io
+import os
+
 import numpy as np
 import pandas as pd
-import yfinance as yf
-from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
+from flask import Flask, render_template, request ,Blueprint
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM
-import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+import yfinance as yf
+from .auth import login_required
 
 app = Blueprint('app', __name__)
 
 @app.route('/predict', methods=['GET', 'POST'])
+@login_required
 def predict():
+    show_results = False
+    plot_url = None
+    prediction = None
+
     if request.method == 'POST':
+        show_results = True
         company = request.form['company']
-        start = '1985-01-01'
-        end = '2022-12-31'
+        start = request.form['start']
+        end = request.form['end']
+        test_start = request.form['test_start']
+        test_end = request.form['test_end']
 
-        data = yf.download(company, start, end)
+        plot_url, prediction = predict_stock_price(company, start, end, test_start, test_end)
 
-        scaler = MinMaxScaler(feature_range=(0, 1))
-        scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
+    return render_template('predict.html', show_results=show_results, plot_url=plot_url, prediction=prediction)
 
-        prediction_days = 60
+def predict_stock_price(company, start, end, test_start, test_end, prediction_days=60):
+    # Load data
+    data = yf.download(company, start=start, end=end)
 
-        x_train = []
-        y_train = []
+    # Prepare data
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    scaled_data = scaler.fit_transform(data['Close'].values.reshape(-1, 1))
 
-        for x in range(prediction_days, len(scaled_data)):
-            x_train.append(scaled_data[x - prediction_days:x, 0])
-            y_train.append(scaled_data[x, 0])
+    train_features, train_targets = prepare_training_data(scaled_data, prediction_days)
 
-        x_train, y_train = np.array(x_train), np.array(y_train)
-        x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    # Build the model
+    model = build_lstm_model(train_features.shape[1])
 
-        model = Sequential()
+    model.compile(optimizer='adam', loss='mean_squared_error')
+    model.fit(train_features, train_targets, epochs=25, batch_size=32)
 
-        model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-        model.add(Dropout(0.2))
-        model.add(LSTM(units=50, return_sequences=True))
-        model.add(Dropout(0.2))
-        model.add(LSTM(units=50))
-        model.add(Dropout(0.2))
-        model.add(Dense(units=1))
+    # Test the model accuracy on existing data
+    test_data = yf.download(company, start=test_start, end=test_end)
+    actual_prices = test_data['Close'].values
 
-        model.compile(optimizer='adam', loss='mean_squared_error')
-        model.fit(x_train, y_train, epochs=25, batch_size=32)
+    plot_url, predicted_prices = plot_test_predictions(model, scaler, company, data, test_data, prediction_days)
 
-        test_start = '2023-01-01'
-        test_end = '2023-06-10'
+    # Predict next day
+    prediction = predict_next_day(model, scaler, data, test_data, prediction_days)
 
-        test_data = yf.download(company, test_start, test_end)
-        actual_prices = test_data['Close'].values
+    return plot_url, prediction
 
-        total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+def prepare_training_data(scaled_data, prediction_days):
+    train_features = []
+    train_targets = []
 
-        model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
-        model_inputs = model_inputs.reshape(-1, 1)
-        model_inputs = scaler.transform(model_inputs)
+    for x in range(prediction_days, len(scaled_data)):
+        train_features.append(scaled_data[x - prediction_days:x, 0])
+        train_targets.append(scaled_data[x, 0])
 
-        x_test = []
-        for x in range(prediction_days, len(model_inputs)):
-            x_test.append(model_inputs[x - prediction_days:x, 0])
+    train_features, train_targets = np.array(train_features), np.array(train_targets)
+    train_features = np.reshape(train_features, (train_features.shape[0], train_features.shape[1], 1))
 
-        x_test = np.array(x_test)
-        x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+    return train_features, train_targets
 
-        predicted_prices = model.predict(x_test)
-        predicted_prices = scaler.inverse_transform(predicted_prices)
+def build_lstm_model(input_shape):
+    model = Sequential()
+    model.add(LSTM(units=50, return_sequences=True, input_shape=(input_shape, 1)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50, return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=1))
 
-        prediction = predicted_prices[-1]
+    return model
 
-        start_time = time.time()
+def plot_test_predictions(model, scaler, company, data, test_data, prediction_days):
+    actual_prices = test_data['Close'].values  # Define actual_prices variable here
+    total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
 
-        plt.figure(figsize=(10, 6))
-        plt.plot(actual_prices, color="red", label=f"Actual {company} Price")
-        plt.plot(predicted_prices, color="blue", label=f"Predicted {company} Price")
-        plt.title(f"{company} Share Price")
-        plt.xlabel('Time')
-        plt.ylabel(f'{company} Share Price')
-        plt.legend()
+    model_inputs = total_dataset[len(total_dataset) - len(test_data) - prediction_days:].values
+    model_inputs = model_inputs.reshape(-1, 1)
+    model_inputs = scaler.transform(model_inputs)
 
-        img = io.BytesIO()
-        plt.savefig(img, format='png')
-        img.seek(0)
-        plot_url = base64.b64encode(img.getvalue()).decode()
+    x_test = []
+    for x in range(prediction_days, len(model_inputs)):
+        x_test.append(model_inputs[x - prediction_days:x, 0])
 
-        elapsed_time = time.time() - start_time
+    x_test = np.array(x_test)
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
 
-        return render_template('predict.html', prediction=prediction, elapsed_time=elapsed_time, plot_url=plot_url)
-    else:
-        return render_template('predict.html')
+    predicted_prices = model.predict(x_test)
+    predicted_prices = scaler.inverse_transform(predicted_prices)
+
+    plt.clf()
+    plt.plot(test_data.index, actual_prices, color="green", label=f"Actual {company} Price")
+    plt.plot(test_data.index, predicted_prices, color="red", label=f"Predicted {company} Price")
+    plt.title(f"{company} Share Price")
+    plt.xlabel('Time')
+    plt.ylabel(f'{company} Share Price')
+    plt.legend()
+
+    img = io.BytesIO()
+    plt.savefig(img, format='png')
+    img.seek(0)
+    plot_url = base64.b64encode(img.getvalue()).decode()
+
+    return plot_url, predicted_prices
+
+def predict_next_day(model, scaler, data, test_data, prediction_days):
+    total_dataset = pd.concat((data['Close'], test_data['Close']), axis=0)
+
+    model_inputs = total_dataset[len(total_dataset) - prediction_days:].values
+    model_inputs = model_inputs.reshape(-1, 1)
+    model_inputs = scaler.transform(model_inputs)
+
+    x_test = model_inputs[-prediction_days:].reshape(1, -1, 1)
+
+    predicted_price = model.predict(x_test)
+    predicted_price = scaler.inverse_transform(predicted_price)
+    next_day_prediction = predicted_price[0][0]
+
+    return next_day_prediction
